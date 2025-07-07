cd70b04924d4ba230e4361de2d4164e29474bb9d
JaskerX <jaskerlp@gmx.de>
2025-07-07 10:33:59 +0200

    implement search parameter notDone

--- components/searchMenu.tsx
+++ components/searchMenu.tsx
@@ -18,6 +18,7 @@ export default function SearchMenu(props: SearchMenuProps) {
 
     const [searchTitle, setSearchTitle] = useState<string>(searchParams.get("title") ?? "");
     const [ignorecase, setIgnorecase] = useState<boolean>(searchParams.get("ignorecase") ? !!Number(searchParams.get("ignorecase")) : true);
+    const [notDone, setNotDone] = useState<boolean>(searchParams.get("notDone") ? !!Number(searchParams.get("notDone")) : false);
 
 
     const handleInputKeyDown = (event: React.KeyboardEvent<HTMLInputElement>) => {
@@ -39,6 +40,7 @@ export default function SearchMenu(props: SearchMenuProps) {
         const params = new URLSearchParams();
         params.set("title", searchTitle.trim());
         params.set("ignorecase", `${Number(ignorecase)}`)
+        params.set("notDone", `${Number(notDone)}`)
         
         onSearch(params);
     }
@@ -66,6 +68,17 @@ export default function SearchMenu(props: SearchMenuProps) {
                 />
                 <label htmlFor="task-completed" className="text-sm ml-[5px]">
                     Groß-/Kleinschreibung beachten
+                </label>
+            </div>
+            <div className="flex flex-row items-center mt-[2px]">
+                <Checkbox
+                    checked={notDone}
+                    onCheckedChange={() => setNotDone(!notDone)}
+                    className="cursor-pointer"
+                    label="Abgeschlossene ausblenden"
+                />
+                <label htmlFor="task-completed" className="text-sm ml-[5px]">
+                    Abgeschlossene ausblenden
                 </label>
             </div>
         </>
