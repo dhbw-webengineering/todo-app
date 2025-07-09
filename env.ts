56c4fab49e2c239c3137fe58bc37ed2f91623233
knoepsim <simon.info.jc@gmail.com>
2025-07-09 20:55:02 +0200

    fixed environment validation

--- env.ts
+++ env.ts
@@ -2,9 +2,7 @@ import 'dotenv/config';
 import { z } from 'zod'
 
 const envSchema = z.object({
-    NEXT_PUBLIC_BACKEND_BASE_PATH: z.string()
+    NEXT_PUBLIC_BACKEND_BASE_PATH: z.string().default("http://localhost:3001")
 })
 
-export const e = envSchema.parse({
-  NEXT_PUBLIC_BACKEND_BASE_PATH: process.env.NEXT_PUBLIC_BACKEND_BASE_PATH
-});
+export const e = envSchema.parse(process.env)
