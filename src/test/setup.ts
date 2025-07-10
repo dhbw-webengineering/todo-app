36a42df9da105eacab36b845049edc0cb9c754a3
Jusitn <justinschuler16@gmail.com>
2025-07-10 11:29:16 +0200

    fix test

--- src/test/setup.ts
+++ src/test/setup.ts
@@ -1,6 +1,9 @@
 import '@testing-library/jest-dom/vitest'
 import { afterEach } from 'vitest'
 import { cleanup } from '@testing-library/react'
+import { config } from 'dotenv'
+
+config({ path: '.env.test' })
 
 afterEach(() => {
   cleanup()
--- vitest.config.ts
+++ vitest.config.ts
@@ -3,20 +3,23 @@ import react from '@vitejs/plugin-react'
 import tsconfigPaths from 'vite-tsconfig-paths'
 
 export default defineConfig({
-    plugins: [react(), tsconfigPaths()],
-    test: {
-        environment: 'jsdom',
-        globals: true,
-        setupFiles: './src/test/setup.ts',
-        coverage: {
-            provider: 'v8',
-            reporter: ['text', 'json', 'html'],
-            exclude: [
-                'node_modules/',
-                'src/test/',
-                '**/*.config.ts',
-                '**/*.d.ts',
-            ],
-        },
+  plugins: [react(), tsconfigPaths()],
+  test: {
+    environment: 'jsdom',
+    globals: true,
+    setupFiles: './src/test/setup.ts',
+    env: {
+      NEXT_PUBLIC_BACKEND_BASE_PATH: 'http://localhost:3001'
     },
+    coverage: {
+      provider: 'v8',
+      reporter: ['text', 'json', 'html'],
+      exclude: [
+        'node_modules/',
+        'src/test/',
+        '**/*.config.ts',
+        '**/*.d.ts',
+      ],
+    },
+  },
 })
